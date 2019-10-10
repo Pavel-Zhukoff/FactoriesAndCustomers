db688565a75218d2c9f81e342c16c1368f7b49d0
Pavel-Zhukoff <pavel.zhuko.ff@yandex.ru>
2019-10-10 21:04:32 +0300

    write Main class

--- src/ru/pavel_zhukoff/Main.java
+++ src/ru/pavel_zhukoff/Main.java
@@ -3,6 +3,11 @@ package ru.pavel_zhukoff;
 public class Main {
 
     public static void main(String[] args) {
+        Store store = new Store();
+        Client client = new Client(store);
+        Factory factory = new Factory(store);
 
+        new Thread(client, "Client").start();
+        new Thread(factory, "Factory").start();
     }
 }
